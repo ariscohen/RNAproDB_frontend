b2d807cafded5390892ace5b62beaa4fc8345c0b
timkartar <timkartar7879@gmail.com>
2023-10-17 13:50:01 -0700

    small css updates

--- src/App.js
+++ src/App.js
@@ -12,12 +12,10 @@ function App() {
       <div className="container">
         <div className="column">
           <h1>3D Structure</h1>
-          <p>Your text goes here. Make sure it's large and modern, and it will wrap as needed.</p>
           <NGLViewer />
         </div>
         <div className="column">
           <h1>Graph Visualization</h1>
-          <p>More text for the right side. You can customize the content as needed.</p>
           <PythonGraph />
         </div>
       </div>
--- src/PythonGraph.js
+++ src/PythonGraph.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 function PythonGraph() {
   return (
-    <div>
-      <iframe
+    <div class="graph_container">
+      <iframe class="responsive-iframe" 
         src="/1ivs.tmp.cif.html"
         title="Embedded HTML"
         width="100%"
         height="1000"
+        scrolling="no"
       ></iframe>
     </div>
   );
