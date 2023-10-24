058afa5f05fb36f32c3439504d7bfa6852ebfb90
timkartar <timkartar7879@gmail.com>
2023-10-24 14:44:44 -0700

    my ip

--- src/NewPythonGraph.js
+++ src/NewPythonGraph.js
@@ -10,8 +10,8 @@ function NewPythonGraph({ dimensions } ) {
   useEffect(() => {
     async function fetchData() {
       try {
-        const response = await fetch(`http://10.136.114.14:8000/rnaprodb/run-script?pdbid=${pdbid}`);
-        // const response = await fetch(`http://10.136.113.92:8000/rnaprodb/run-script?pdbid=${pdbid}`);
+        //const response = await fetch(`http://10.136.114.14:8000/rnaprodb/run-script?pdbid=${pdbid}`);
+        const response = await fetch(`http://10.136.113.92:8000/rnaprodb/run-script?pdbid=${pdbid}`);
         // Check if the response has content and if it's JSON
         const contentType = response.headers.get("content-type");
         if (contentType && contentType.indexOf("application/json") !== -1) {
