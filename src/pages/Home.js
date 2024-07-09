0adb7f70a5c81ba0db34103f57b2c7d9f651ca82
Ari Cohen <aricohen@yahoo.com>
2024-07-08 18:15:15 -0700

    attempt 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -200,12 +200,13 @@ const Home = () => {
                 </div>
               </div>
               <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px' }}>
-              <div style={{ marginTop: '5px', marginBottom: '5px' }}>
+
+                <div id="right_column" onClick={window.reset_graph_colors}>
+                <div style={{ marginTop: '5px', marginBottom: '5px' }}>
                                 <strong>Rotate:</strong> drag + left click &nbsp;
                                 <strong>Translate:</strong> drag + right click &nbsp;
                                 <strong>Zoom:</strong> scroll
                             </div>
-                <div id="right_column" onClick={window.reset_graph_colors}>
                   <NewPythonGraph
                     pdbid={pdbid}
                     setGraphData={setGraphData}
