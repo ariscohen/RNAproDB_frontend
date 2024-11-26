c9406c77c994270b8af7a65c82b3c3b2f3fa7b51
Ari Cohen <aricohen@yahoo.com>
2024-11-25 16:41:32 -0800

    search text

--- src/pages/Upload.js
+++ src/pages/Upload.js
@@ -56,7 +56,7 @@ function Upload() {
 
     return (
         <div className="upload-container">
-            <h1 className="upload-title">Upload</h1>
+            <h1 className="upload-title">Upload a CIF or PDB file</h1>
             <p>The file should be a CIF or PDB biological assembly file containing nucleic acids. Files up to 10 MB are currently supported to ensure informative visualizations.</p>
             <form onSubmit={handleSubmit} className="upload-form">
                 <input type="file" onChange={handleFileChange} accept=".cif, .pdb" className="file-input"/>
